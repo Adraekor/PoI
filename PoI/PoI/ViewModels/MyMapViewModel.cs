06b3ff32312136200f6692d68c317ae14db67129
akmenos54 <kleinnico54@gmail.com>
2019-03-06 12:20:04 +0100

    ajout de la geoloc

--- PoI/PoI/ViewModels/MyMapViewModel.cs
+++ PoI/PoI/ViewModels/MyMapViewModel.cs
@@ -6,33 +6,72 @@ using System.Linq;
 using Xamarin.Forms.Maps;
 using Xamarin.Forms;
 using System.Collections.ObjectModel;
+using Plugin.Geolocator;
+using System.Threading.Tasks;
+using System.Diagnostics;
 
 namespace PoI.ViewModels
 {
     
     public class MyMapViewModel : BindableBase
 	{
-      public static Map map = new Map(MapSpan.FromCenterAndRadius(new Position(49.094928, 6.229883), Distance.FromMiles(0.5))){IsShowingUser = true,VerticalOptions = LayoutOptions.FillAndExpand};
-
+        public Map Map { get; private set; }
+        static Position test;
         public MyMapViewModel()
         {
+            Map = new Map(
+                MapSpan.FromCenterAndRadius(
+                new Position(49.094928, 6.229883),
+                Distance.FromMiles(0.3)))
+            {
+                //IsShowingUser = true,
+                VerticalOptions = LayoutOptions.FillAndExpand
+            };
+            //MapSpan.FromCenterAndRadius(new Position(49.094928, 6.229883), Distance.FromMiles(0.5))){IsShowingUser = true,VerticalOptions = LayoutOptions.FillAndExpand};
 
-            var position1 = new Position(49.094928, 6.229883);
+
+             CallingGPS();
+            
+        }
+
+
+        async void CallingGPS()
+        {
+            var locator = CrossGeolocator.Current;
+            Position position;
+            locator.DesiredAccuracy = 120;
+            
+            if (!CrossGeolocator.IsSupported)
+                position = new Position();
+            if (!CrossGeolocator.Current.IsGeolocationEnabled)
+                position = new Position();
+            if (!CrossGeolocator.Current.IsGeolocationAvailable)
+                position = new Position();
+
+            try
+            {
+                var ex = await locator.GetPositionAsync(TimeSpan.FromSeconds(120), null, false);
+                position = new Position(ex.Latitude, ex.Longitude);
+            }
+            catch (Exception e)
+            {
+                position = new Position();
+            }
+
+            test = new Position(  position.Latitude,position.Longitude);
 
             var pin1 = new Pin
             {
                 Type = PinType.Place,
-                Position = position1,
-                Label = "MEILLEUR FAC DE LA GALAXY",
+                //Position = position1,
+                //Position = new Position(49.094928, 6.229883),//position.Latitude, position.Longitude),
+                Position = position,
+            Label = "MEILLEUR FAC DE LA GALAXY",
                 Address = "www.google.fr",
-                
+
             };
 
-
-
-            map.Pins.Add(pin1); 
+            Map.Pins.Add(pin1);                                                       
         }
-
-
-	}
+    }
 }
--- PoI/PoI/Views/MyMap.xaml.cs
+++ PoI/PoI/Views/MyMap.xaml.cs
@@ -10,29 +10,9 @@ namespace PoI.Views
 {
     public partial class MyMap : ContentPage
     {
-        static Position current = new Position(49.094928, 16.229883);
         public MyMap()
         {
-            InitializeComponent();
-            BindingContext = new MyMapViewModel();
-            var map = MyMapViewModel.map; 
-            Content = map;
-            /*
-            var position1 = new Position(49.094928, 6.229883);
-
-            var pin1 = new Pin
-            {
-                Type = PinType.Place,
-                Position = position1,
-                Label = "MEILLEUR FAC DE LA GALAXY",
-                Address = "www.google.fr",
-            };
-
-
-
-            map2.Pins.Add(pin1);  */
-
-              
+            InitializeComponent();     
         }
     }
 }
